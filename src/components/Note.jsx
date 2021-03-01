fec45e5b94273b6c8246939f6caefc9c097ecebe
mathildehaugum <mathilde_haugum@hotmail.com>
2021-03-01 12:23:28 +0100

    Add delete button and connect to functionality implemented by App component

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-function Note() {
+function Note(props) {
   return (
     <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button
+        onClick={() => {
+          props.onDeleteNote(props.id);
+        }}
+      >
+        DELETE
+      </button>
     </div>
   );
 }
