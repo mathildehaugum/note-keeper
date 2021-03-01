f3f1f03c590ed6aff3cd85827c2655f358d3c0e6
mathildehaugum <mathilde_haugum@hotmail.com>
2021-03-01 12:21:21 +0100

    Allow user to add and delete notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <Note />
+      <CreateArea onAddNote={addNote} />
+      {notes.map((note, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDeleteNote={deleteNote}
+        />
+      ))}
       <Footer />
     </div>
   );
