afcd643533aaa3d450bcd6819a5fc4476282cd9f
mathildehaugum <mathilde_haugum@hotmail.com>
2021-03-05 17:36:07 +0100

    Add state to textarea

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
+
+  const [isExpanded, setExpanded] = useState(false);
+
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
+
+  function handleClick() {
+    setExpanded(true);
+  }
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -23,26 +33,32 @@ function CreateArea(props) {
       title: "",
       content: ""
     });
+    setExpanded(false);
     event.preventDefault();
   }
 
   return (
     <div>
-      <form>
-        <input
+      <form className="create-note">
+        {isExpanded ? <input
           onChange={handleChange}
           name="title"
           placeholder="Title"
           value={note.title}
-        />
+        /> : null}
         <textarea
+          onClick={handleClick}
           onChange={handleChange}
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded ? "3" : "1"}
           value={note.content}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
