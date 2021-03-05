ff87d92b2f6913fac031431103b21efbcb28582f
mathildehaugum <mathilde_haugum@hotmail.com>
2021-03-05 17:35:10 +0100

    Add icon to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import HighlightIcon from '@material-ui/icons/Highlight';
 
 function Header() {
   return (
     <header>
-      <h1>Notater</h1>
+      <h1><HighlightIcon /> Notater</h1>
     </header>
   );
 }
